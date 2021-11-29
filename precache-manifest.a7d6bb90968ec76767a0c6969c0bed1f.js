0a2111cb7343d2ccf9ab08017adbd72cc25311ee
sergeytymoshchuk87 <sergeytymoshchuk87@gmail.com>
2021-11-30 00:43:52 +0300

    upload the project

---
+++ precache-manifest.a7d6bb90968ec76767a0c6969c0bed1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cab2fe210eb5fb868775aa762cd2226",
+    "url": "/Cryptocurrency/index.html"
+  },
+  {
+    "revision": "7c22eceafeeec8ec7d8b",
+    "url": "/Cryptocurrency/static/css/main.3de1ab71.chunk.css"
+  },
+  {
+    "revision": "6ce60c4439d624e24439",
+    "url": "/Cryptocurrency/static/js/2.c42bc561.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/Cryptocurrency/static/js/2.c42bc561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c22eceafeeec8ec7d8b",
+    "url": "/Cryptocurrency/static/js/main.ef7e7d91.chunk.js"
+  },
+  {
+    "revision": "23e4ac164048ae5bb2bc",
+    "url": "/Cryptocurrency/static/js/runtime-main.2624632a.js"
+  }
+]);
